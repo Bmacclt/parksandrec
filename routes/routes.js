b07860f111bcc17936b46762653e907f2f5df7da
John Dekis <johndekis@gmail.com>
2018-04-04 19:42:22 -0400

    added to routes, server, others

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,21 @@
 // shaun -- routes 
 // express router 
 // git database 
+var db = require("../models");
+
+module.exports = function(app) {
+    app.get("/api/authors", function(req, res) {
+      // Here we add an "include" property to our options in our findAll query
+      // We set the value to an array of the models we want to include in a left outer join
+      // In this case, just db.Post
+      db.Parks.findAll({
+        //include: [db.Post]
+      }).then(function(dbParks) {
+        res.json(dbParks);
+      });
+    });
+  ;
+ 
+  
+  };
+
