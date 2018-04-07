cadb6ae9430447ace8f1a68eb86289eba445ec2e
shazamba <Shaunshah24@gmail.com>
2018-04-07 13:35:47 -0400

    5

--- public/assets/logic.js
+++ public/assets/logic.js
@@ -1,7 +1,31 @@
-var googleMapsClient = require('@google/maps').createClient({
-    key: 'AIzaSyCLOMXJ-NKk4X28qsVQ1XjoaPsSW2HzMCs'
-  });
 
+//for the map, didn't work.
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: {lat: -34.397, lng: 150.644}
+        });
+        var geocoder = new google.maps.Geocoder();
+
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+      }
+
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = '1100 S tryon st, charlotte, nc';
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
 
 
 // click handlers and receive info -- John 
@@ -25,29 +49,30 @@ var googleMapsClient = require('@google/maps').createClient({
 
 
 
-    var geocoder;
-    var map;
-    function initialize() {
-      geocoder = new google.maps.Geocoder();
-      var mapOptions = {
-        zoom: 8,
-        center: latlng
-      }
-      map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    }
+    // var geocoder;
+    // var map;
+    // function initialize() {
+    //   geocoder = new google.maps.Geocoder();
+    //   var latlng = new googleMapsClient.Latlng(-34.397, 150.644);
+    //   var mapOptions = {
+    //     zoom: 8,
+    //     center: latlng
+    //   }
+    //   map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    // }
   
-    function codeAddress() {
-      var address = 28203;
-      geocoder.geocode( { 'address': address}, function(results, status) {
-        if (status == 'OK') {
-          map.setCenter(results[0].geometry.location);
-          var marker = new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location
-          });
-        } else {
-          alert('Geocode was not successful for the following reason: ' + status);
-        }
-      });
-    }
+    // function codeAddress() {
+    //   var address = '1000 S Tryon st, charlotte, nc';
+    //   geocoder.geocode( { 'address': address}, function(results, status) {
+    //     if (status == 'OK') {
+    //       map.setCenter(results[0].geometry.location);
+    //       var marker = new google.maps.Marker({
+    //           map: map,
+    //           position: results[0].geometry.location
+    //       });
+    //     } else {
+    //       alert('Geocode was not successful for the following reason: ' + status);
+    //     }
+    //   });
+    // }
   
