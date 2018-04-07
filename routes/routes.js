26afbd91f2abccc191544c156d8792999aa55fc1
John Dekis <johndekis@gmail.com>
2018-04-07 11:15:27 -0400

    routes and add.handlebars added

--- routes/routes.js
+++ routes/routes.js
@@ -19,6 +19,22 @@ module.exports = function(app) {
       });
     });
 
+
+    app.get("/parks/:id", function(req, res) {
+      db.Park.findOne({}).then(function(dbPark) {
+      // console.log(dbPark[0].dataValues.parkName);
+      // console.log(dbPark[1].dataValues.parkName);
+      // console.log(dbPark[2].dataValues.parkName);
+      // console.log(dbPark.length);
+      var hbsObject = {
+        parks: dbPark
+      };
+              
+      res.render("index", hbsObject);
+    });
+  });
+
+
    //Parks A-Z 
    //====================================================== 
     app.get("/all-parks", function(req, res) {
@@ -58,6 +74,7 @@ module.exports = function(app) {
       res.render("add", hbsObject);
       });
     });
+
   };
 
 
