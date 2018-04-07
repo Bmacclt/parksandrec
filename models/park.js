1518d973f32526e50733ac09c6f90a6255e95bb0
Brian Strylowski <brianstry@hotmail.com>
2018-04-07 11:25:49 -0400

    fixing tables by adding lat and lon

--- models/park.js
+++ models/park.js
@@ -22,6 +22,14 @@ module.exports = function (sequelize, DataTypes) {
             allowNull: false,
             defaultValue: false
 
+        },
+        lat: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        lon: {
+          type: DataTypes.STRING,
+          allowNull: false
         }
 
     });
