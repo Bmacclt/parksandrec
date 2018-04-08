53dbf6f80c01f220c456305912f1cfc56ff26826
shazamba <Shaunshah24@gmail.com>
2018-04-08 17:27:02 -0400

    maps update

--- public/assets/googlemaps.js
+++ public/assets/googlemaps.js
@@ -1,12 +1,5 @@
-// var publicConfig = {
-//     key: 'AIzaSyCLOMXJ-NKk4X28qsVQ1XjoaPsSW2HzMC',
-//     stagger_time: 1000, // for elevationPath
-//     encode_polylines: false,
-//     secure: true, // use https
-//     proxy: 'http://127.0.0.1:9999' // optional, set a proxy for HTTP requests
-//     };
-//     var gmAPI = new GoogleMapsAPI(publicConfig);
-
+var marker;
+var Bballmarkers = [];
 
     var map = new google.maps.Map(document.getElementById("map"), {
         center: new google.maps.LatLng(55.6810038, 12.6033823),
@@ -17,3 +10,27 @@
         streetViewControl: false,
         zoomControl: false
     });
+
+//where i am currently stuck-shaun
+function arrayInfo(place, arrayMarker){
+
+}
+
+    //code for each checkbox
+
+    // pool array.
+    function setBballMap(map) {
+        for (var i = 0; i < Bballmarkers.length; i++) {
+          Bballmarkers[i].setMap(map);
+        }
+      }	
+	
+	function toggleBball() {
+	var chkPoolLayer = document.getElementById("basketball"); 
+	if (basketball.checked === true)
+		//Turn layer on 
+		setBballMap(map);          
+	else
+		//Turn layer off 
+		setBballMap(null);  
+	}
