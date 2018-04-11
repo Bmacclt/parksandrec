711344c633eab2f4583408eb63f49ffbdb7226f3
Brian Strylowski <brianstry@hotmail.com>
2018-04-11 19:42:04 -0400

    adding api calls

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -38,4 +38,134 @@ module.exports = function (app) {
         })
     });
 
+    app.get("/api/dogpark", function (req, res) {
+        db.AllPark.findAll({
+            where: {
+                dogPark: true
+            }
+        }).then(function (dbPark) {
+            var hbsObject = {
+                parks: dbPark
+            };
+            res.json(hbsObject);
+        })
+    });
+
+    app.get("/api/golf", function (req, res) {
+        db.AllPark.findAll({
+            where: {
+                golf: true
+            }
+        }).then(function (dbPark) {
+            var hbsObject = {
+                parks: dbPark
+            };
+            res.json(hbsObject);
+        })
+    });
+
+    app.get("/api/playground", function (req, res) {
+        db.AllPark.findAll({
+            where: {
+                playground: true
+            }
+        }).then(function (dbPark) {
+            var hbsObject = {
+                parks: dbPark
+            };
+            res.json(hbsObject);
+        })
+    });
+
+    app.get("/api/soccer", function (req, res) {
+        db.AllPark.findAll({
+            where: {
+                soccer: true
+            }
+        }).then(function (dbPark) {
+            var hbsObject = {
+                parks: dbPark
+            };
+            res.json(hbsObject);
+        })
+    });
+
+    app.get("/api/tennis", function (req, res) {
+        db.AllPark.findAll({
+            where: {
+                tennis: true
+            }
+        }).then(function (dbPark) {
+            var hbsObject = {
+                parks: dbPark
+            };
+            res.json(hbsObject);
+        })
+    });
+
+    app.get("/api/trails", function (req, res) {
+        db.AllPark.findAll({
+            where: {
+                trails: true
+            }
+        }).then(function (dbPark) {
+            var hbsObject = {
+                parks: dbPark
+            };
+            res.json(hbsObject);
+        })
+    });
+
+    app.get("/api/vendingmachines", function (req, res) {
+        db.AllPark.findAll({
+            where: {
+                vendingMachines: true
+            }
+        }).then(function (dbPark) {
+            var hbsObject = {
+                parks: dbPark
+            };
+            res.json(hbsObject);
+        })
+    });
+
+    app.get("/api/views", function (req, res) {
+        db.AllPark.findAll({
+            where: {
+                views: true
+            }
+        }).then(function (dbPark) {
+            var hbsObject = {
+                parks: dbPark
+            };
+            res.json(hbsObject);
+        })
+    });
+
+    app.get("/api/watersports", function (req, res) {
+        db.AllPark.findAll({
+            where: {
+                waterSports: true
+            }
+        }).then(function (dbPark) {
+            var hbsObject = {
+                parks: dbPark
+            };
+            res.json(hbsObject);
+        })
+    });
+
+    app.get("/api/workoutgear", function (req, res) {
+        db.AllPark.findAll({
+            where: {
+                workoutGear: true
+            }
+        }).then(function (dbPark) {
+            var hbsObject = {
+                parks: dbPark
+            };
+            res.json(hbsObject);
+        })
+    });
+
 };
